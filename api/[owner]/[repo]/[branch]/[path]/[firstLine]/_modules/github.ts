f49fe248ed5b6654d49d3c2683585044a660c5fc
InkoHX <inkohx@gmail.com>
2020-08-07 20:24:52 +0900

    fix: Remove line breaks in highlighted code

--- api/[owner]/[repo]/[branch]/[path]/[firstLine]/_modules/github.ts
+++ api/[owner]/[repo]/[branch]/[path]/[firstLine]/_modules/github.ts
@@ -10,6 +10,7 @@ export const fetchHighlightedCode = async (page: Page): Promise<HighlightedCode>
   const finalPath = await page.evaluate(() => document.getElementsByClassName('final-path')[0]?.textContent)
   const highlightedLines = await page.$$('.blob-code.blob-code-inner.highlighted')
     .then(elements => Promise.all(elements.map(element => element.evaluate(element => element.textContent ?? ''))))
+    .then(codes => codes.map(code => code.replace(/\n|\r\n|\r/u, '')))
   
   return {
     extension: finalPath ? extname(finalPath).substring(1) : null,
